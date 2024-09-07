e7aa8be544325a60674db6116e1a9e4e80266775
amar-kakde <amar.kakde@outlook.com>
2024-09-07 22:19:26 +0530

    forms.py and html pages for crud operations created
    todo_add.html created
    todo_delete.html created
    todo_update.html created

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class TodoModel(models.Model):
+    todo        = models.CharField(max_length=255)
+    description = models.CharField(max_length=526)
+    status      = models.BooleanField(default=False)
+    created     = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
 
+from .models import TodoModel
+from .forms import TodoForm
+
 # Create your views here.
 
 def index(request):
     return render(request, "todo/index.html", {})
+
+def todo_add(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+    form = TodoForm()
+
+    return render(request, "todo/todo_add.html", {"form":form})    
+
+def todo_delete(request):
+    pass
+
+
+def todo_update(request):
+    pass
+
+
+def todo_list(request):
+    pass
+
+
